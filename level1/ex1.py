f852d06c2f361cdb5764014780ca9a682a3e8082
ssar@nate.com <ssar@nate.com>
2021-05-06 16:05:45 +0900

    1강 파이썬 기본 문법 - 리스트, 문자열, 숫자, 튜플

---
+++ level1/ex1.py
@@ -0,0 +1,66 @@
+# # HelloWorld 파스칼표기법
+# # helloWorld 낙타표기법
+# # hello_world 언더스코어 (파이썬)
+
+
+# # 타입추론
+# # 모든 겅시 오브젝트
+# # 인터프리터
+
+
+# #1. 숫자
+# a = 1
+# b = 1.2
+# c = 4e5
+
+
+# print(a) 
+# print(b) 
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# # print(type(a))
+# # print(type(b))
+# # print(type(c))
+
+
+# # 연산자
+# e= 3
+# f = 4
+
+# print(e**f)
+
+# print(f//e)
+
+
+# # 문자
+# s1 = "안녕하세요"
+# s3 = '''
+# 안녕하세요
+# '''
+
+# print(s3)
+
+# s4 = s1 + s3
+# print(s4)
+
+# s5 = '홍길동'
+
+# print(s5+ '님 안녕하세요')
+
+# print(f"{s5}님 안녕핳세요")
+# print('='*50)
+
+# 3,슬라이싱 :
+
+str1 = '가나다라마'
+
+print(str1[0:2])
+
+print(str1[-1])
+print(str1[1:])
+
+
+
